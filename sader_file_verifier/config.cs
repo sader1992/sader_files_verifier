f6e722e49d3f3bc2da7ffbd67cdbc5545bbb360c
Sader Fawall <sader1992@hotmail.com>
2019-04-27 15:54:29 +0200

    clear config

--- sader_file_verifier/config.cs
+++ sader_file_verifier/config.cs
@@ -7,9 +7,9 @@ namespace sader_file_verifier
 {
     public class config
     {
-        public readonly string INFO_TEXT_URL = "http://arenaro.ragnarok.gs/opatch/Information.txt"; //direct link to the information file
+        public readonly string INFO_TEXT_URL = "http://127.0.0.1/verifier/Information.txt"; //direct link to the information file
         public readonly string INFO_TEXT_NAME = "Information.txt";  //name of the information file
-        public readonly string FILES_URL = "http://arenaro.ragnarok.gs/opatch/verify/"; //link to the folder of the information file
+        public readonly string FILES_URL = "http://127.0.0.1/verifier/verify/"; //link to the folder of the information file
         public readonly string CLINT_EXE = "Ragnarok";  //name of the exe you use as client , the program will not run if it found the client running.
     }
 }
